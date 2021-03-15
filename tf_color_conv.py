4f2f6c3f18fccc88a715055a1596b2ee0f31cb8e
m4-k131 <40889634+m4-k131@users.noreply.github.com>
2021-03-15 13:31:56 +0100

    Update tf_color_conv.py

--- tf_color_conv.py
+++ tf_color_conv.py
@@ -37,6 +37,7 @@ def get_conversion_fn(colorspace='yuv'):
     def rgb2lab(img):
         img=experimental.color.rgb_to_lab(img).numpy()
         img/=cs_array
+        return img
 
     def lab2rgb(image):
         img=image*cs_array
